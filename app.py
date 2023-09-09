b4dd7c2fc3d96323bfd518eed5d4986b7a9d1190
khoffan <khoffan222@gmail.com>
2023-09-09 23:37:00 +0700

    implement to date

--- app.py
+++ app.py
@@ -2,18 +2,24 @@ import random
 import json
 import uuid
 import datetime
+import time
 
 
 user_data = {"users":[]}
 count = 1
-
+num = 1
+current_date = datetime.datetime.now()
 while count < 11:
     username = random.choice(["john", "may", "king", "max"])
     location = random.choice(["USA", "Maxcio", "Canada", "England", "Thailand", "Agentina", "Korea", "japan"])
     status = random.choice([True,False])
-    time = datetime.datetime.now()
-    format_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    # timeDate = datetime.datetime.now()
+    current_date -= datetime.timedelta(days=1)
+    current_dates = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    # print(current_date)
+    
     ids = str(uuid.uuid4())
+    count += 1
     user_data["users"].append({
         "id": ids,
         "name": username,
@@ -21,10 +27,10 @@ while count < 11:
         "locate":{
             "location": location,
         },
-        "date": format_time,
+        "date": current_dates,
     })
-    count += 1
 
+# print(user_data)
 with open("users_data_fornat.json", "w") as file_json:
     json.dump(user_data, file_json, indent=2)
 
