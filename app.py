780936cc4430cb14b5d1a54ac4a4bfa5daab6cd7
khoffan <khoffan222@gmail.com>
2023-11-16 19:56:40 +0700

    update api

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from user_matching import matching
 import json
 
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route("/", methods=["GET"])
 def welcome_my_api():
     return "Welcome to my API!"
 @app.route("/api/matching", methods=['POST'])
 def matchingAPI():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    if 'customers' not in data or 'riders' not in data:
-            return jsonify({'error': 'Invalid request. "customers" and "riders" are required.'}), 400
+        if 'customers' not in data :
+            return jsonify({'error': 'Invalid request. "customers" must be a list of customer data.'}), 400
 
-    customers = data['customers']
+        customers = data['customers']
 
-    matched = matching(customers)
-    return jsonify({'matches': matched})
+        # Add more input validation if needed
+
+        matched = matching(customers)
+        return jsonify({'matches': matched})
+
+    except Exception as e:
+        return jsonify({'error': f'Server error: {str(e)}'}), 500
 
 
 if __name__ in '__main__':
--- user_matching.py
+++ user_matching.py
@@ -14,15 +14,15 @@ def matching(customer):
     customers_ids = set()
     for c in customer:
         for r in riders:
-            if r['id'] not in rider_ids and c['id'] not in customers_ids and c['location'] == r['location']:
+            if r['id'] not in rider_ids and c['id'] not in customers_ids and ( c['location'] == r['location'] or r["location"] == "รับทุกงาน"):
                 matches.append({'customer_id': c['id'],'customer_name': c['name'], 'rider_id': r['id'],'rider_name': r['name']})
                 rider_ids.add(r["id"])
                 customers_ids.add(c["id"])
-
+                break
     return matches
 
-matching_result = matching(customers)
-print(matching_result)
+# matching_result = matching(customers)
+# print(matching_result)
 
 # riders = [riders for rider in riders if rider["id"] not in rider_ids]
 
