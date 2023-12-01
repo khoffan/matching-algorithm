da8eebd0dcf16f1bd30ee2f0af2024c03097aea3
khoffan <khoffan222@gmail.com>
2023-12-01 21:27:21 +0700

    add getlocation rider

--- app.py
+++ app.py
@@ -84,5 +84,35 @@ def getRiderId(id_):
         return jsonify({"error": f"Server error: {str(e)}"}), 500
 
 
+@app.route("/api/riderlocate/<string:locate>", methods=["GET"])
+def getRiderlLocate(locate):
+    try:
+        data = sort_rider
+        rider = []
+        if not data:
+            return (
+                jsonify(
+                    {"error": 'Invalid request. "riders" must be a list of rider data.'}
+                ),
+                400,
+            )
+        for r in data:
+            if r['location'] == locate:
+                rider.append(r)
+            
+        if not rider:
+            return (
+                jsonify(
+                    {
+                        "error": f'Invalid request. "rider location: {locate}" must be a list of rider data.'
+                    }
+                ),
+                400,
+            )
+        return jsonify({"riders": rider})
+    except Exception as e:
+        return jsonify({"error": f"Server error: {str(e)}"}), 500
+
+
 if __name__ in "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
